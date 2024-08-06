177cc077d92f58d3f80b69eadb021718d968cdd6
Githoony <kihoonleee@naver.com>
2024-08-06 09:49:45 +0900

    Home

--- SteamReact/steampunk-app/src/steam-jwt/pages/Home.jsx
+++ SteamReact/steampunk-app/src/steam-jwt/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import '../pages/SteamNewsCss.css';
 
 import axios from 'axios';
@@ -9,32 +8,6 @@ import YoutubeVideos from './YoutubeVideos';
 
 
 const Home = () => {
-
-    const [games, setGames] = useState([]);
-    const [news, setNews] = useState([]);
-    const [chart, setChart] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [message, setMessage] = useState('');
-    const navigate = useNavigate();
-    const { isLogin } = useContext(LoginContext);
-    const { logout } = useContext(LoginContext);
-    // const key = "AIzaSyCOaXfLbU-uxGuK4UXWVGO80QuhzOXQ7Ds";
-    const key = "YOUR_KEY";
-
-
-    useEffect(() => {
-        axios({
-            method: "get",
-            url: "http://localhost:8080/game/testGames"
-        })
-            .then((response) => {
-                setGames(response.data);
-                console.log("games ======== ", games);
-            })
-
-    }, []);
-
-
     return (
         <>
             <SideBar/>
@@ -47,7 +20,6 @@ const Home = () => {
                 <section className="slider">
                     <div className="recommended">
                         <FeaturedGames />
-
                     </div>
                 </section>
                 <section className="content-grid">
