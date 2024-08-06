36b1438c2fd8fd29ac22806d7ba08473eb8c34ec
Githoony <kihoonleee@naver.com>
2024-08-06 09:42:23 +0900

    Css 수정

--- SteamReact/steampunk-app/src/steam-jwt/pages/Home.jsx
+++ SteamReact/steampunk-app/src/steam-jwt/pages/Home.jsx
@@ -1,27 +1,17 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../pages/SteamNewsCss.css';
-import { useNavigate } from 'react-router-dom';
-import { LoginContext } from '../contexts/LoginContextProvider';
+
 import axios from 'axios';
-import { Carousel, Col } from 'react-bootstrap';
 import ChartTest from './ChartTest';
 import FeaturedGames from './FeaturedGames';
 import SideBar from '../components/sidebar/SideBar';
+import YoutubeVideos from './YoutubeVideos';
 
 
 const Home = () => {
 
-    const [youtube, setYoutube] = useState({ items: [] });
     const [games, setGames] = useState([]);
-    const [news, setNews] = useState([]);
-    const [chart, setChart] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [message, setMessage] = useState('');
-    const navigate = useNavigate();
-    const { isLogin } = useContext(LoginContext);
-    const { logout } = useContext(LoginContext);
-    const key = "AIzaSyCOaXfLbU-uxGuK4UXWVGO80QuhzOXQ7Ds";
-    // const key = "YOUR_KEY";
+
 
 
     useEffect(() => {
@@ -36,29 +26,6 @@ const Home = () => {
 
     }, []);
 
-    // 유튜브 정보 가져오기
-    useEffect(() => {
-
-        if (!key) {
-            setMessage('키 없음');
-            return;
-        }
-
-        axios({
-            method: "get",
-            url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=Steam+game+trailertype=video&key=" + key,
-        })
-            .then((response) => {
-
-                setYoutube(response.data);
-            })
-            .catch((error) => {
-                console.error('유튜브 데이터 요청 중 오류 발생!', error);
-                setMessage('유튜브 데이터 요청 중 오류 발생');
-            });
-    }, [key]);
-
-    const hasData = youtube.items.length > 0;
 
     return (
         <>
@@ -80,24 +47,8 @@ const Home = () => {
                         <ChartTest />
                     </div>
                     <div className="grid-item video">
-                        {hasData ? (<iframe
-                            width="482"
-                            height="270"
-                            src={`https://www.youtube.com/embed/${youtube.items[0].id.videoId}`}
-                            title={`https://www.youtube.com/embed/${youtube.items[0].snippet.title}`}
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>) : (<p>로딩중</p>)}
-
-                        {hasData ? (<iframe
-                            width="482"
-                            height="270"
-                            src={`https://www.youtube.com/embed/${youtube.items[1].id.videoId}`}
-                            title={`https://www.youtube.com/embed/${youtube.items[1].snippet.title}`}
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>) : (<p>로딩중</p>)}
-
+                       <YoutubeVideos />
                     </div>
-
                 </section>
             </main>
         </>
---
+++ SteamReact/steampunk-app/src/steam-jwt/pages/YoutubeVideos.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const YoutubeVideos = () => {
+
+    const [youtube, setYoutube] = useState({ items: [] });
+    const [message, setMessage] = useState('');
+
+    // const key = "AIzaSyCOaXfLbU-uxGuK4UXWVGO80QuhzOXQ7Ds";
+    const key = "YOUR_KEY";
+
+    // 유튜브 정보 가져오기
+    useEffect(() => {
+
+
+        if (!key) {
+            setMessage('키 없음');
+            return;
+        }
+
+        axios({
+            method: "get",
+            url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=Steam+game+trailertype=video&key=" + key,
+        })
+            .then((response) => {
+
+                setYoutube(response.data);
+            })
+            .catch((error) => {
+                console.error('유튜브 데이터 요청 중 오류 발생!', error);
+                setMessage('유튜브 데이터 요청 중 오류 발생');
+            });
+    }, [key]);
+
+    const hasData = youtube.items.length > 0;
+
+    return (
+        <div className="grid-item video">
+            {hasData ? (<iframe
+                width="482"
+                height="270"
+                src={`https://www.youtube.com/embed/${youtube.items[0].id.videoId}`}
+                title={`https://www.youtube.com/embed/${youtube.items[0].snippet.title}`}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>) : (<p>로딩중</p>)}
+
+            {hasData ? (<iframe
+                width="482"
+                height="270"
+                src={`https://www.youtube.com/embed/${youtube.items[1].id.videoId}`}
+                title={`https://www.youtube.com/embed/${youtube.items[1].snippet.title}`}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>) : (<p>로딩중</p>)}
+
+        </div>
+    );
+};
+
+export default YoutubeVideos;
