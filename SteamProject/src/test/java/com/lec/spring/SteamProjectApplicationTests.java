51d72f414d93e36c20c8957a0d4f76fef4cd5339
Githoony <kihoonleee@naver.com>
2024-08-19 17:08:52 +0900

    dd

--- SteamProject/src/test/java/com/lec/spring/SteamProjectApplicationTests.java
+++ SteamProject/src/test/java/com/lec/spring/SteamProjectApplicationTests.java
@@ -1,13 +1,5 @@
 package com.lec.spring;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SteamProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
