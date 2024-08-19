252fc8885c1b3e76ab10f32a104315413272982e
Githoony <kihoonleee@naver.com>
2024-08-19 16:54:48 +0900

    2

--- SteamProject/src/test/java/com/lec/spring/jwt/JWTUtilTest.java
+++ SteamProject/src/test/java/com/lec/spring/jwt/JWTUtilTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+
 class JWTUtilTest {
 
 }
