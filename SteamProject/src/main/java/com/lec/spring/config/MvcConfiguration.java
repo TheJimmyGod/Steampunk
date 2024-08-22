a5588b307cc1876490a027405f36e20d06810702
TheJimmyGod <minjang115@gmail.com>
2024-08-22 11:22:41 +0900

    Updated user

--- SteamProject/src/main/java/com/lec/spring/config/MvcConfiguration.java
+++ SteamProject/src/main/java/com/lec/spring/config/MvcConfiguration.java
@@ -1,7 +1,10 @@
 package com.lec.spring.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +12,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfiguration implements WebMvcConfigurer {
     @Value("${cors.allowed-origins}")
     private String[] corsAllowedOrigins;
-
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry
